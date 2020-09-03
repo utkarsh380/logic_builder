9fa66b6cf590e3a5498e6744e9bc06f2c2278cbf
utkarsh380 <patelutkarsh341@gmail.com>
2020-09-03 11:35:23 +0530

    file upload

---
+++ FindDuplicate.php
@@ -0,0 +1,25 @@
+<?php
+//https://leetcode.com/problems/find-the-duplicate-number/submissions/
+class FindDuplicate {
+
+/**
+ * @param Integer[] $nums
+ * @return Integer
+ */
+function findDuplicate($nums) {
+     $len= sizeof($nums);
+    for ($i = 0; $i < $len; $i++) {
+        $nums[$nums[$i] % $len] += $len;
+    }
+    $max = 0;
+    $val = -1;
+    for ($i = 0; $i < $len; $i++) {
+        if ($nums[$i] > $max) {
+            $max = $nums[$i];
+            $val = $i;
+        }
+    }
+    return $val; 
+}
+}
+?>
